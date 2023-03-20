b7c94604367ba5997036403e70a1deff4bedc262
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-03-19 22:41:56 -0300

    modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
+import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +16,16 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  })],
+  }), image()],
+  vite: {
+    plugins: [],
+    resolve: {
+      alias: {
+        '#': path.resolve(__dirname, './src')
+      }
+    },
+    optimizeDeps: {
+      allowNodeBuiltins: true
+    }
+  },
 });
