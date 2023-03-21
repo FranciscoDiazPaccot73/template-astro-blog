f5743197f592f6988668197b015cc096b01ac068
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-03-20 22:44:33 -0300

    end template

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,7 @@
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const TWITTER = 'https://twitter.com/pancho_xor';
+export const GITHUB = "https://github.com/withastro/astro";
+
+export const BG_MAIN_COLOR = '#1f2937';
+export const MAIN_TEXT_COLOR = '#fff';
